647ab8f2b26e4d4eb5bb468eaf620e3bfd887bbc
Dinesh <37468808+itsdinesh@users.noreply.github.com>
2026-08-07 07:18:58 +0800

    Use stdbuf for autoninja output buffering

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ cp $SCRIPT_DIR/args.gn out/Default/args.gn
 gn gen out/Default # gn args out/Default; echo 'treat_warnings_as_errors = false' >> out/Default/args.gn
 mkdir -p out/tmp out/release
 
-autoninja -C out/Default chrome_public_apk
+stdbuf -oL -eL autoninja -C out/Default chrome_public_apk
 mv $(find out/Default/apks -name 'Chrome*.apk') out/tmp/$VERSION-x86_64.apk
 
 export PATH=$PWD/third_party/jdk/current/bin/:$PATH
